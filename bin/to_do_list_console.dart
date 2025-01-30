6a438d6088906d1d23bc247cc8d106c96ad0eb1e
Frejus DASSI <frejohope@gmail.com>
2025-01-30 11:09:46 +0100

    Amélioration de l'interface utilisateur de la console To-Do List en ajoutant des messages d'invite et en intégrant les fonctionnalités de marquage et de suppression des tâches.

--- bin/to_do_list_console.dart
+++ bin/to_do_list_console.dart
@@ -1,10 +1,13 @@
 import 'package:to_do_list_console/to_do_list_console.dart' as to_do_list_console;
 import 'dart:io';
+import 'package:to_do_list_console/to_do_list_console.dart';
 
 void main(List<String> arguments) {
 
   // print('Hello world: ${to_do_list_console.calculate()}!');
+  Task task = Task("description", false);
 
+  
   while (true) {
     print("\n--- To-Do List ---");
     print("1. Afficher les tâches");
@@ -13,25 +16,30 @@ void main(List<String> arguments) {
     print("4. Supprimer une tâche");
     print("5. Quitter");
     print("Choisissez une option :");
+    print("Que voulez-vous faire ?");
 
-    var choix = int.parse("Que voulez-vous faire ?");
+     String choix = stdin.readLineSync()!;
 
     switch (choix) {
-      case 1:
+      case '1':
         to_do_list_console.ToDoList().listTasks();
         break;
-      case 2:
+      case '2':
         print("Entrez la description de la tâche :");
         String description = stdin.readLineSync()!;
         to_do_list_console.ToDoList().addTask(description);
         break;
-      case 3:
-        // to_do_list_console.supprimerTache();
+      case '3':
+        print("Entrez le numéro de la tâche à marquer comme terminée :");
+        int index = int.parse(stdin.readLineSync()!);
+        to_do_list_console.ToDoList().markTaskAsDone(index);
         break;
-      case 4:
-        // to_do_list_console.marquerTacheCommeFait();
+      case '4':
+        print("Entrez le numéro de la tâche à supprimer :");
+        int index = int.parse(stdin.readLineSync()!);
+        to_do_list_console.ToDoList().deleteTask(index);
         break;
-      case 5:
+      case '5':
         print("Au revoir !");
         return;
       default:
