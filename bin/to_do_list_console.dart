90e49d12e63fa5f1c1bee4fd9f147f31e6bdfe92
Frejus DASSI <frejohope@gmail.com>
2025-01-30 10:53:25 +0100

    Mise à jour du menu de l'application To-Do List pour améliorer l'interface utilisateur et ajouter des options de gestion des tâches.

--- bin/to_do_list_console.dart
+++ bin/to_do_list_console.dart
@@ -1,31 +1,35 @@
 import 'package:to_do_list_console/to_do_list_console.dart' as to_do_list_console;
+import 'dart:io';
 
-void main(List<String> arguments, String taches) {
+void main(List<String> arguments) {
 
   // print('Hello world: ${to_do_list_console.calculate()}!');
 
   while (true) {
-    print("Le menu de notre application");
-    print("1. Ajouter une tâche");
-    print("2. Lister les tâches");
-    print("3. Supprimer une tâche");
-    print("4. Marquer une tâche comme faite");
+    print("\n--- To-Do List ---");
+    print("1. Afficher les tâches");
+    print("2. Ajouter une tâche");
+    print("3. Marquer une tâche comme terminée");
+    print("4. Supprimer une tâche");
     print("5. Quitter");
+    print("Choisissez une option :");
 
     var choix = int.parse("Que voulez-vous faire ?");
 
     switch (choix) {
       case 1:
-        to_do_list_console.ToDoList().ajouterTache(taches);
+        to_do_list_console.ToDoList().listTasks();
         break;
       case 2:
-        
+        print("Entrez la description de la tâche :");
+        String description = stdin.readLineSync()!;
+        to_do_list_console.ToDoList().addTask(description);
         break;
       case 3:
-        
+        // to_do_list_console.supprimerTache();
         break;
       case 4:
-        
+        // to_do_list_console.marquerTacheCommeFait();
         break;
       case 5:
         print("Au revoir !");
